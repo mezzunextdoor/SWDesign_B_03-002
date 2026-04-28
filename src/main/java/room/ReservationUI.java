eb5ca139efd8d757360d3eb6f545ce86315cf380
nayeju <nana692579@gmail.com>
2026-04-28 16:49:33 +0900

    Create ReservationUI.java

---
+++ src/main/java/room/ReservationUI.java
@@ -0,0 +1,12 @@
+package room;
+
+public class ReservationUI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		예약정보 _rev = new 예약정보();
+			System.out.println("예약 총비용 : " + _rev.예약("hong", "room001", 20260428, 3));
+
+	}
+
+}
